e1cde90cf1e171592030ef9b81593fd833b157b0
EJDACUBA-cpu <jhoshcunanan@gmail.com>
2025-08-22 22:11:48 +0800

    Added labels

--- CalculatorApplication/Form1.Designer.cs
+++ CalculatorApplication/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(244, 167);
+            this.label2.Location = new System.Drawing.Point(244, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 1;
